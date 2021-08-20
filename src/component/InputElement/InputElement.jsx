f94fb329427b5980d60875fc460ff5a9e4831ea4
SumitK27 <sumitkajbaje@gmail.com>
2021-08-20 13:22:10 +0530

    Initial Commit

---
+++ src/component/InputElement/InputElement.jsx
@@ -0,0 +1,26 @@
+import styles from "./inputElement.module.css";
+
+const InputElement = ({ title, zoom, setTitle, setZoom }) => {
+    return (
+        <div className={styles.input}>
+            <input
+                style={{ margin: "20px" }}
+                name="range"
+                type="range"
+                min={20}
+                max={70}
+                value={zoom}
+                onChange={(e) => setZoom(e.target.value)}
+            />
+
+            <input
+                style={{ margin: "20px", fontSize: "22px" }}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                type="text"
+            />
+        </div>
+    );
+};
+
+export default InputElement;
